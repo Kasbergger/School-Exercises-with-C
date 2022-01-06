21e90dde0a508f7e8ab28325ef1aac85981e2232
Kasbergger <84065832+Kasbergger@users.noreply.github.com>
2022-01-06 08:59:59 +0100

    Create 2-Print_userinput_integer.c

---
+++ Programiz exercises/exercises 1-10/2-Print_userinput_integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int j;
+    //Ask for an integer and stores it in j
+    printf("Please enter a hole number: ");
+    scanf("%d", &j);
+    
+    //Prints the integer out
+    printf("\nYou entered the number: %d", j);
+    
+    return 0;
+}
